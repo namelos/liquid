c45da3218b604552afee3a7d9fa46171b602f3f2
namelos <namelos@outlook.com>
2018-08-24 17:21:57 +0800

    modularized client creation

---
+++ client.js
@@ -0,0 +1,38 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
+import { withClientState } from 'apollo-link-state'
+
+import { reduceQuery } from './utils'
+
+export function configureClient() {
+  const cache = new InMemoryCache()
+
+  const stateLink = withClientState({
+    cache,
+    defaults: { n: 1 },
+    resolvers: {
+      Mutation: {
+        increment: reduceQuery('query { n }', ({ n }) => ({ n: n + 1 })),
+        decrement: reduceQuery('query { n }', ({ n }) => ({ n: n - 1 })),
+      }
+    }
+  })
+
+  return new ApolloClient({
+    link: ApolloLink.from([
+      stateLink,
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.map(({ message, locations, path }) => {
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+          })
+        }
+        if (networkError) console.log(`[Network error]: ${networkError}`)
+      })
+    ]),
+    cache: cache
+  })
+}
+
--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { onError } from 'apollo-link-error'
-import { ApolloLink } from 'apollo-link'
-import { withClientState } from 'apollo-link-state'
 import { ApolloProvider } from 'react-apollo'
 
-import { ql, reduceQuery } from './utils'
+import { ql } from './utils'
+import { configureClient } from './client'
 
-const cache = new InMemoryCache()
-
-const stateLink = withClientState({
-  cache,
-  defaults: { n: 1 },
-  resolvers: {
-    Mutation: {
-      increment: reduceQuery('query { n }', ({ n }) => ({ n: n + 1 })),
-      decrement: reduceQuery('query { n }', ({ n }) => ({ n: n - 1 })),
-    }
-  }
-})
-
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    stateLink,
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }) => {
-          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-        })
-      }
-      if (networkError) console.log(`[Network error]: ${networkError}`)
-    })
-  ]),
-  cache: cache
-})
+const client = configureClient()
 
 const Counter = ql('query { n }', {
   decrement: 'mutation { decrement @client }',
